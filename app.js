5ab243df37dee607a16fa9702100d8971efd8e89
simonescoca <simone.scoca@icloud.com>
2023-12-12 02:09:39 +0100

    #

--- app.js
+++ app.js
@@ -9,51 +9,51 @@ cInput("YT Download Video - Inserisci l\'URL")
         if (YTvideoURL.startsWith("https://www.youtube.com/watch")) {
             downloadVideo(YTvideoURL);
     
-        } else console.log("URL non valido")
+        } else console.log("URL non valido");
     })
     .catch((err) => {
-        console.log(err.message)
+        console.log(err.message);
     });
 
 
 function downloadVideo(YTvideoURL) {
-    console.log("> download del video in corso...")
+    console.log("> download del video in corso...");
     const videoFilePath = "video.mp4";
     ytdl(YTvideoURL, { filter: "audioandvideo", quality: "highestaudio", format: "mp4" })
         .pipe(fs.createWriteStream(videoFilePath))
         .on(("error"), (err) => {
-            console.log("errore durante il download del video: " + err);
-            return
+            console.log("errore durante il download del video: " + err.message);
+            return;
         })
         .on("finish", () => {
-            console.log("> download video.mp4 completato")
+            console.log("> download video.mp4 completato");
             convertToAudio(YTvideoURL, videoFilePath);
         });
 }
 
 
-async function convertToAudio(YTvideoURL, videoFilePath) {
+function convertToAudio(YTvideoURL, videoFilePath) {
     console.log("> conversione a file audio.mp3 in corso...")
-    let titolo = "";
-    let autore = "";
 
-    await ytdl.getInfo(YTvideoURL).then((info) => {
-        console.log("> recupero titolo e autore del video...")
-        titolo = info.videoDetails.title;
-        autore = info.videoDetails.author.name;
-    });
+    ytdl.getInfo(YTvideoURL)
+        .then((info) => {
+            console.log("> recupero titolo e autore del video...")
 
-    ffmpeg(videoFilePath)
-        .output(`${titolo + " - " + autore}.mp3`)
-        .on('end', () => {
-            fs.unlink(videoFilePath, (err) => {
-                if (err) {
-                    console.error('Errore durante l\'eliminazione del file video: ' + err);
-                    return;
-                }
-            });
-        })
-        .on('error', (err) => console.error('Errore durante la conversione a file.mp3: ' + err))
-        .on("end", () => console.log("> conversione completata"))
-        .run();
+            const titolo = info.videoDetails.title;
+            const autore = info.videoDetails.author.name;
+            
+            ffmpeg(videoFilePath)
+                .output(`${titolo + " - " + autore}.mp3`)
+                .on("end", () => {
+                    fs.unlink(videoFilePath, (err) => {
+                        if (err) {
+                            console.error("Errore durante l'eliminazione del file video: " + err.message);
+                            return;
+                        }
+                    });
+                })
+                .on("error", (err) => console.error("Errore durante la conversione a file.mp3: " + err.message))
+                .on("end", () => console.log("> conversione completata"))
+                .run();
+        });
 }
