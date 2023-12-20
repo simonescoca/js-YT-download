e8c6de4c485da5d520a6843fa1db462868eff4a4
simonescoca <simone.scoca@icloud.com>
2023-12-20 11:22:36 +0100

    #

--- utils/_dirproj.js
+++ utils/_dirproj.js
@@ -1,3 +1,9 @@
 const _dirproj = (__dirname.replace("/utils" , ""));
 
+/**
+ * per creare un'app eseguibile con pkg non puoi usare __dirname, perché le azioni sui file system sono relegate all'ambiente dello snapshot
+ */
+// const path = require("path");
+// const _dirproj = (path.dirname(process.execPath).replace("/utils" , ""));
+
 module.exports = _dirproj;
