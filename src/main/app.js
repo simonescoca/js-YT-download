ed16bbd9b267a81d7a7f919953cb504d88e3aee0
simonescoca <simone.scoca@icloud.com>
2024-05-22 12:12:45 +0200

    fixed typo

--- src/main/app.js
+++ src/main/app.js
@@ -1,12 +1,12 @@
 const getInfos = require("../functions/getInfos");
 const download = require("../functions/download");
-const converToAudio = require("../functions/convertToAudio");
+const convertToAudio = require("../functions/convertToAudio");
 
 getInfos()
 .then((res) => {
     download(res.url, res.author, res.title)
     .then((videopath) => {
-        converToAudio(videopath);
+        convertToAudio(videopath);
     })
 })
 .catch((err) => {
