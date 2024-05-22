3b391ff5d006a71adcf326b1ad615a0233f75af2
simonescoca <simone.scoca@icloud.com>
2024-05-22 17:44:54 +0200

    removed useless comments

--- src/functions/download.js
+++ src/functions/download.js
@@ -1,39 +1,3 @@
-// const ytdl = require("ytdl-core");
-// const fs = require("fs");
-// const os = require("os");
-
-// function download(url, author, title) {
-
-//     return new Promise((resolve, reject) => {
-//         const now = new Date();
-//         const timestamps = Math.round(now.getTime() / 1000);
-//         const filepath = `${os.homedir}/Downloads/${title}-${author}-${timestamps}.mp4`;
-        
-//         console.log(`> il video verrà scaricato in ${os.homedir}/Downloads`);
-//         console.log("> download del video in corso...");
-
-//         const videoStream = ytdl(url, { filter: "audioandvideo", quality: "highestaudio", format: "mp4" });
-//         const fileStream = fs.createWriteStream(filepath);
-
-//         videoStream.pipe(fileStream);
-
-//         fileStream.on("error", (err) => {
-//             console.log("> errore durante il download del video", err);
-//             reject(err);
-//         });
-
-//         fileStream.on("finish", () => {
-//             console.log(`> download di ${title}-${author}-${timestamps}.mp4 completato!`);
-//             resolve(filepath);
-//         });
-//     });
-
-// }
-
-// module.exports = download;
-
-
-
 const ytdl = require("ytdl-core");
 const fs = require("fs");
 const os = require("os");
@@ -47,7 +11,6 @@ function download(url, author, title) {
         const filepath = `${os.homedir()}/Downloads/${title}-${author}-${timestamps}.mp4`;
         const videoStream = ytdl(url, { filter: "audioandvideo", quality: "highestaudio", format: "mp4" });
 
-        // Initialize progress bar
         let bar;
         videoStream.on('response', (res) => {
             const totalSize = res.headers['content-length'];
