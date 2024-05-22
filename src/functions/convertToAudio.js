b774298ef235137a7076a5d0e4ef7febc69d9a6d
simonescoca <simone.scoca@icloud.com>
2024-05-22 12:12:22 +0200

    added progress usage & logic

--- src/functions/convertToAudio.js
+++ src/functions/convertToAudio.js
@@ -1,18 +1,66 @@
+// const ffmpeg = require("fluent-ffmpeg");
+// const os = require("os");
+// const progressBar = require("progress");
+
+// function convertToAudio(videopath) {
+
+//     let filename = videopath.split("/");
+//     filename = filename[filename.length - 1].replace(".mp4", "");
+ 
+//     const proc = ffmpeg(videopath)
+//         .output(`${os.homedir()}/Downloads/${filename}.mp3`)
+//         .on("error", (err) => console.error("> errore durante la conversione a file.mp3", err))
+//         .on("end", () => console.log(`> conversione completata!\n> trovi i files in ${os.homedir()}/Downloads`));
+
+//     // Initialize progress bar
+//     const bar = new progressBar('> conversione a file audio in corso... [:bar] :percent :etas', {
+//         width: 40,
+//         complete: '=',
+//         incomplete: ' ',
+//         renderThrottle: 100,
+//         total: 100  // Set total to 100 to represent percentage
+//     });
+
+//     proc.on('progress', (progress) => {
+//         bar.update(progress.percent / 100);
+//     });
+
+//     proc.run();
+// }
+
+// module.exports = convertToAudio;
+
 const ffmpeg = require("fluent-ffmpeg");
 const os = require("os");
+const ProgressBar = require("progress");
 
 function convertToAudio(videopath) {
 
     let filename = videopath.split("/");
     filename = filename[filename.length - 1].replace(".mp4", "");
-
-    console.log(`> conversione a file audio in corso...`);
  
-    ffmpeg(videopath)
-    .output(`${os.homedir}/Downloads/${filename}.mp3`)
-    .on("error", (err) => console.error("> errore durante la conversione a file.mp3", err))
-    .on("end", () => console.log(`> conversione completata!\n> trovi il tuo file.mp3 in ${os.homedir}/Downloads`))
-    .run();
+    const proc = ffmpeg(videopath)
+        .output(`${os.homedir()}/Downloads/${filename}.mp3`)
+        .on("error", (err) => console.error("> errore durante la conversione a file.mp3", err))
+        .on("end", () => {
+            bar.update(1); // Force the progress bar to complete
+            console.log(`> conversione completata!\n> trovi i tuoi files in ${os.homedir()}/Downloads`);
+        });
+
+    // Initialize progress bar
+    const bar = new ProgressBar('> conversione a file audio in corso... [:bar] :percent :etas', {
+        width: 40,
+        complete: '=',
+        incomplete: ' ',
+        renderThrottle: 100,
+        total: 100  // Set total to 100 to represent percentage
+    });
+
+    proc.on('progress', (progress) => {
+        bar.update(progress.percent / 100);
+    });
+
+    proc.run();
 }
 
 module.exports = convertToAudio;
--- src/functions/download.js
+++ src/functions/download.js
@@ -1,20 +1,70 @@
+// const ytdl = require("ytdl-core");
+// const fs = require("fs");
+// const os = require("os");
+
+// function download(url, author, title) {
+
+//     return new Promise((resolve, reject) => {
+//         const now = new Date();
+//         const timestamps = Math.round(now.getTime() / 1000);
+//         const filepath = `${os.homedir}/Downloads/${title}-${author}-${timestamps}.mp4`;
+        
+//         console.log(`> il video verrà scaricato in ${os.homedir}/Downloads`);
+//         console.log("> download del video in corso...");
+
+//         const videoStream = ytdl(url, { filter: "audioandvideo", quality: "highestaudio", format: "mp4" });
+//         const fileStream = fs.createWriteStream(filepath);
+
+//         videoStream.pipe(fileStream);
+
+//         fileStream.on("error", (err) => {
+//             console.log("> errore durante il download del video", err);
+//             reject(err);
+//         });
+
+//         fileStream.on("finish", () => {
+//             console.log(`> download di ${title}-${author}-${timestamps}.mp4 completato!`);
+//             resolve(filepath);
+//         });
+//     });
+
+// }
+
+// module.exports = download;
+
+
+
 const ytdl = require("ytdl-core");
 const fs = require("fs");
 const os = require("os");
+const progressBar = require("progress");
 
 function download(url, author, title) {
 
     return new Promise((resolve, reject) => {
         const now = new Date();
         const timestamps = Math.round(now.getTime() / 1000);
-        const filepath = `${os.homedir}/Downloads/${title}-${author}-${timestamps}.mp4`;
-        
-        console.log(`> il video verrà scaricato in ${os.homedir}/Downloads`);
-        console.log("> download del video in corso...");
-
+        const filepath = `${os.homedir()}/Downloads/${title}-${author}-${timestamps}.mp4`;
         const videoStream = ytdl(url, { filter: "audioandvideo", quality: "highestaudio", format: "mp4" });
-        const fileStream = fs.createWriteStream(filepath);
 
+        // Initialize progress bar
+        let bar;
+        videoStream.on('response', (res) => {
+            const totalSize = res.headers['content-length'];
+            bar = new progressBar('> download del video in corso... [:bar] :percent :etas', {
+                width: 40,
+                complete: '=',
+                incomplete: ' ',
+                renderThrottle: 100,
+                total: parseInt(totalSize)
+            });
+        });
+
+        videoStream.on('data', (chunk) => {
+            bar.tick(chunk.length);
+        });
+
+        const fileStream = fs.createWriteStream(filepath);
         videoStream.pipe(fileStream);
 
         fileStream.on("error", (err) => {
