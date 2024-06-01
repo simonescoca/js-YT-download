930f16833e60d25ae14174d8dc9b559cc34bdaa4
simonescoca <simone.scoca@icloud.com>
2024-06-02 01:30:02 +0200

    added check for -fv query by user

--- src/functions/getInfos.js
+++ src/functions/getInfos.js
@@ -3,26 +3,29 @@ const ytdl = require("ytdl-core");
 
 async function getInfos() {
     let check = false;
+    let isFullVideo = false;
 
     while (!check) {
         try {
-            const url = await input("> YT Download Video - Inserisci l'URL : ");
-
-            if (url.startsWith("https://www.youtube.com/watch")) {
-                const info = await ytdl.getBasicInfo(url);
-                check = true;
-
-                const infos = {
-                    url: url,
-                    author: info.videoDetails.author.name,
-                    title: info.videoDetails.title
-                };
-
-                return infos;
-
-            } else {
-                console.log("> URL non valido");
+            let url = await input("> YT Download - Inserisci l'URL : ");
+            
+            if (url.startsWith("-fv https://www.youtube.com/watch")){
+                isFullVideo = true;
+                url = url.slice(4);
             }
+            
+            const info = await ytdl.getBasicInfo(url);
+            check = true;
+
+            const infos = {
+                url: url,
+                author: info.videoDetails.author.name,
+                title: info.videoDetails.title,
+                fullVideo: isFullVideo
+            };
+
+            return infos;
+
         } catch (err) {
             console.log("> URL non valido", err);
         }
